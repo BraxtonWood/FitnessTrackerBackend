fcf0c5f153e1af082195ddc7e8d960256d328041
Braxton Wood <brakwood@gmail.com>
2023-02-12 13:24:22 -0700

    changed db connection AGAAIN

--- db/client.js
+++ db/client.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 const client = new Pool({
   user: "BraxtonWood",
   host: "db.bit.io",
-  database: "postgresql://BraxtonWood:v2_3z48R_khVTG84Y7mF2zJArvfMxQ3g@db.bit.io:5432/BraxtonWood/FitnessTracker",
+  database: "BraxtonWood/FitnessTracker",
   password: "v2_3z48R_khVTG84Y7mF2zJArvfMxQ3g",
   port: 5432,
   ssl: true
