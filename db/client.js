04f53167e6f720e08da6bf4e73bebfdbccbc491d
Braxton Wood <brakwood@gmail.com>
2023-02-12 11:42:40 -0700

    changed pkg and db connection

--- db/client.js
+++ db/client.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
+//const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
 
 const client = new Pool({
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  user: "BraxtonWood",
+  host: "db.bit.io",
+  database: "postgresql://BraxtonWood:v2_3z3x9_vix3METmW9PiewcXZPXyYKs@db.bit.io:5432/BraxtonWood/FitnessTracker",
+  password: "v2_3z3x9_vix3METmW9PiewcXZPXyYKs",
+  port: 5432,
+  ssl: true
 });
 
 module.exports = client;
